10092fe6326b48eea13615424469c79efb163dbb
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-14 22:49:56 +0900

    feat: DashBoard 컨포넌트 생성

---
+++ millionaire/src/components/DashBoard.jsx
@@ -0,0 +1,61 @@
+export default function DashBoard() {
+	return (
+		<div className="flex-none w-[65%] mt-[20px]">
+			<h1 className="mb-[10px] text-[40px]">Dash Board</h1>
+			<div className="w-[100%] mb-[50px]">
+				<h1 className="text-[25px]">Achievement</h1>
+				<hr className="mb-[5px]"></hr>
+				<ul className="text-[20px]">
+					<li className="flex">
+						<div className="w-[50%]">hyuim</div>
+						<div className="w-[20%]">admin</div>
+						<div className="w-[15%] text-center">23</div>
+						<div className="w-[15%] text-center">40</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">phan</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[15%] text-center">40</div>
+						<div className="w-[15%] text-center">100</div>
+					</li>
+				</ul>
+			</div>
+			<div className="w-[100%]">
+				<h1 className="text-[25px]">Penalty</h1>
+				<hr className="mb-[5px]"></hr>
+				<ul className="text-[20px]">
+					<li className="flex">
+						<div className="w-[50%]">hyuim</div>
+						<div className="w-[20%]">admin</div>
+						<div className="w-[15%] text-center">3000</div>
+						<div className="w-[15%] text-center">5000</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">soohlee</div>
+						<div className="w-[20%]">manager</div>
+						<div className="w-[15%] text-center">3000</div>
+						<div className="w-[15%] text-center">5000</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">sungmiki</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[15%] text-center">3000</div>
+						<div className="w-[15%] text-center">5000</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">jonhan</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[15%] text-center">3000</div>
+						<div className="w-[15%] text-center">5000</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">junssong</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[15%] text-center">3000</div>
+						<div className="w-[15%] text-center">5000</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
+}
