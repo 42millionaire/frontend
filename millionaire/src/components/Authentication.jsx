6821ff7c96b89c7298bf9b2c3e7a762aea393f04
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-14 22:48:51 +0900

    feat: Authentication 컨포넌트 생성

---
+++ millionaire/src/components/Authentication.jsx
@@ -0,0 +1,67 @@
+export default function Authentication() {
+	return (
+		<div className="flex-none w-[65%] mt-[20px]">
+			<h1 className="mb-[10px] text-[40px]">Authentication</h1>
+			<div className="mb-[50px] w-[100%]">
+				<h1 className="text-[25px]">Task</h1>
+				<hr className="mb-[5px]"></hr>
+				<ul className="text-[20px]">
+					<li className="flex">
+						<div className="w-[35%]">다이렉트x 완성하기</div>
+						<div className="w-[20%]">hyuim</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+					<li className="flex">
+						<div className="w-[35%]">스프링 마스터</div>
+						<div className="w-[20%]">soohlee</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+					<li className="flex">
+						<div className="w-[35%]">Webserv 완성하기</div>
+						<div className="w-[20%]">sungmiki</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+					<li className="flex">
+						<div className="w-[35%]">세후1억 프로젝트</div>
+						<div className="w-[20%]">jonhan</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+					<li className="flex">
+						<div className="w-[35%]">클러스터 10시간 있기</div>
+						<div className="w-[20%]">junssong</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+				</ul>
+			</div>
+			<div className="w-[100%]">
+				<h1 className="text-[25px]">Appeels</h1>
+				<hr className="mb-[5px]"></hr>
+				<ul className="text-[20px]">
+					<li className="flex">
+						<div className="w-[35%]">토익 단어 100개 외우기</div>
+						<div className="w-[20%]">younghyle</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+					<li className="flex">
+						<div className="w-[35%]">편입수학 편미분</div>
+						<div className="w-[20%]">phan</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+					<li className="flex">
+						<div className="w-[35%]">주지스 마스터</div>
+						<div className="w-[20%]">hcho2</div>
+						<div className="w-[33%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅ ❎</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
+}
