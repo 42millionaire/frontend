28241dc1d9e98a1df49f63ae6a095c32d789ec1f
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-14 22:49:31 +0900

    feat: MemberManage 컨포넌트 생성

---
+++ millionaire/src/components/MemberManage.jsx
@@ -0,0 +1,64 @@
+export default function MemberManage() {
+	return (
+		<div className="flex-none w-[65%] mt-[20px]">
+			<h1 className="mb-[10px] text-[40px]">Member Management</h1>
+			<div className="mb-[50px] w-[100%]">
+				<h1 className="text-[25px]">Members</h1>
+				<hr className="mb-[5px]"></hr>
+				<ul className="text-[20px]">
+					<li className="flex">
+						<div className="w-[50%]">hyuim</div>
+						<div className="w-[20%]">admin</div>
+						<div className="w-[10%] text-center">⬆️</div>
+						<div className="w-[10%] text-center">⬇️</div>
+						<div className="w-[10%] text-center">❌</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">soohlee</div>
+						<div className="w-[20%]">manager</div>
+						<div className="w-[10%] text-center">⬆️</div>
+						<div className="w-[10%] text-center">⬇️</div>
+						<div className="w-[10%] text-center">❌</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">sungmiki</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[10%] text-center">⬆️</div>
+						<div className="w-[10%] text-center">⬇️</div>
+						<div className="w-[10%] text-center">❌</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">jonhan</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[10%] text-center">⬆️</div>
+						<div className="w-[10%] text-center">⬇️</div>
+						<div className="w-[10%] text-center">❌</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">junssong</div>
+						<div className="w-[20%]">member</div>
+						<div className="w-[10%] text-center">⬆️</div>
+						<div className="w-[10%] text-center">⬇️</div>
+						<div className="w-[10%] text-center">❌</div>
+					</li>
+				</ul>
+			</div>
+			<div className="w-[100%]">
+				<h1 className="text-[25px]">Join Request</h1>
+				<hr className="mb-[5px]"></hr>
+				<ul className="text-[20px]">
+					<li className="flex">
+						<div className="w-[50%]">seoson</div>
+						<div className="w-[40%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅</div>
+					</li>
+					<li className="flex">
+						<div className="w-[50%]">donglee2</div>
+						<div className="w-[40%]">2024.09.03 23:02</div>
+						<div className="w-[10%] text-center">✅</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
+}
