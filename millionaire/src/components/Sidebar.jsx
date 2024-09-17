ab7924653a906f459720c63c0439b670d5dda723
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-17 21:59:15 +0900

    feat: SideBar 컨포넌트 추가

--- millionaire/src/components/Sidebar.jsx
+++ millionaire/src/components/Sidebar.jsx
@@ -2,14 +2,15 @@ import PAGE_TITLES from "../constants/PAGE_TITLES";
 
 const Sidebar = ({ setPage }) => (
 	<div className="w-[23%] mt-[20px] ml-[20px]">
-		<h1 className="mb-[50px] text-[36px]">세후1억 해적단</h1>
+		<h1 className="mb-[50px] mt-[20px] text-[30px]">☠️세후1억 해적단☠️</h1>
 		<nav>
 			<ul className="text-[20px]">
 				{Object.entries(PAGE_TITLES).map(([key, title]) => (
-					<li key={key} onClick={() => setPage(key)}>
+					<li key={key} onClick={() => setPage(key)} className="hover:text-orange-400 cursor-pointer  w-fit">
 						{title}
 					</li>
 				))}
+				<a href="/" className="hover:text-orange-400">Admin out</a>
 			</ul>
 		</nav>
 	</div>
