12dd35ef75c1ad2d3e7bd33e1a69ddaa6eeafed8
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-17 21:53:41 +0900

    fix: 요정에러 수정중

--- millionaire/src/components/Authentication.jsx
+++ millionaire/src/components/Authentication.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
+import AdminContentBlock from "./AdminContentBlock";
+import AdminBody from "./AdminBody";
+import BASE_URL from "../constants/URL";
 
 const Authentication = () => {
 	const [data, setData] = useState([]);
+	// const [appeals, setAppeals] = useState([]);
+	// setAppeals(res.appeals);
+
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const response = await fetch("../data/authentication.json", {
-					headers: {
-						"Content-Type": "application/json",
-					},
+				const response = await fetch(`${BASE_URL}/task/01`, {
+
+					method: "GET"
 				});
 				const res = await response.json();
 				setData(res.task);
@@ -18,56 +23,50 @@ const Authentication = () => {
 		};
 		fetchData();
 	}, []);
-
-	const renderData = () => {
-		return data.map(
-			(item) =>
-				item.status === "pend" && (
-					<li key={item.id} className="flex">
-						<div className="w-[35%] mr-[1%] whitespace-nowrap overflow-hidden text-ellipsis">
-							{item.content}
-						</div>
-						<div className="w-[20%]">{item.member_name}</div>
-						<div className="w-[33%]">{item.created_time}</div>
-						<div className="w-[10%] text-center">✅ ❎</div>
-					</li>
+	
+	const renderPendings = () => {
+		return (
+			<ul className="text-[20px]">{
+				data.map(
+				(item) =>
+					item.status === "pend" && (
+						<li key={item.id} className="flex hover:text-orange-400">
+							<div className="w-[35%] mr-[1%] whitespace-nowrap overflow-hidden text-ellipsis cursor-pointer">
+								{item.content}
+							</div>
+							<div className="w-[20%]">{item.member_name}</div>
+							<div className="w-[33%]">{item.created_time}</div>
+							<div className="w-[10%] text-center">✅ ❎</div>
+						</li>
 				),
-		);
+				)}
+			</ul>
+		)
 	};
+	// const renderAppeals = () => {
+	// 	return (
+	// 		<ul className="text-[20px]">
+	// 			{appeals.map(
+	// 			(item) =>(
+	// 					<li key={item.id} className="flex hover:text-orange-400">
+	// 						<div className="w-[35%] mr-[1%] whitespace-nowrap overflow-hidden text-ellipsis cursor-pointer">
+	// 							{item.content}
+	// 						</div>
+	// 						<div className="w-[20%]">{item.member_name}</div>
+	// 						<div className="w-[33%]">{item.created_at}</div>
+	// 						<div className="w-[10%] text-center">✅ ❎</div>
+	// 					</li>
+	// 				),
+	// 			)}
+	// 		</ul>
+	// 	);
+	// };
 
 	return (
-		<div className="w-[65%] mt-[20px]">
-			<h1 className="mb-[10px] text-[40px]">Authentication</h1>
-			<div className="mb-[50px] w-[100%]">
-				<h1 className="text-[25px]">Task</h1>
-				<hr className="mb-[5px]"></hr>
-				<ul className="text-[20px]">{renderData()}</ul>
-			</div>
-			<div className="w-[100%]">
-				<h1 className="text-[25px]">Appeels</h1>
-				<hr className="mb-[5px]"></hr>
-				<ul className="text-[20px]">
-					<li className="flex">
-						<div className="w-[35%]">토익 단어 100개 외우기</div>
-						<div className="w-[20%]">younghyle</div>
-						<div className="w-[33%]">2024.09.03 23:02</div>
-						<div className="w-[10%] text-center">✅ ❎</div>
-					</li>
-					<li className="flex">
-						<div className="w-[35%]">편입수학 편미분</div>
-						<div className="w-[20%]">phan</div>
-						<div className="w-[33%]">2024.09.03 23:02</div>
-						<div className="w-[10%] text-center">✅ ❎</div>
-					</li>
-					<li className="flex">
-						<div className="w-[35%]">주지스 마스터</div>
-						<div className="w-[20%]">hcho2</div>
-						<div className="w-[33%]">2024.09.03 23:02</div>
-						<div className="w-[10%] text-center">✅ ❎</div>
-					</li>
-				</ul>
-			</div>
-		</div>
+		<AdminBody title={"Authentication"}>
+			<AdminContentBlock title={"Pendings"} contents={renderPendings}/>
+			{/* <AdminContentBlock title={"Appeals"} contents={renderAppeals}/> */}
+		</AdminBody>
 	);
 };
 
