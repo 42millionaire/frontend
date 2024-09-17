6f220bab8a9ce1f9bc3e47473831dde3f535b74f
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-17 22:00:16 +0900

    feat: DashBoard 컨포넌트 추가

--- millionaire/src/components/DashBoard.jsx
+++ millionaire/src/components/DashBoard.jsx
@@ -1,61 +1,44 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import AdminContentBlock from "./AdminContentBlock";
+import AdminBody from "./AdminBody";
+
 export default function DashBoard() {
+	const [penalties, setPenalties] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("../data/penalty.json", {
+					headers: {
+						"Content-Type": "application/json",
+					},
+				});
+				const res = await response.json();
+				setPenalties(res.members);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+	}, []);
+	
+	const renderPenalties = () => {
+		return (
+			<ul className="text-[20px]">{
+				penalties.map(
+				(item) =>(
+				<li key={item.id} className="flex">
+					<div className="w-[50%]">{item.member_name}</div>
+					<div className="w-[30%]">{item.penalty}</div>
+				</li>
+				),
+				)}
+			</ul>
+		)
+	};
 	return (
-		<div className="w-[65%] mt-[20px]">
-			<h1 className="mb-[10px] text-[40px]">Dash Board</h1>
-			<div className="w-[100%] mb-[50px]">
-				<h1 className="text-[25px]">Achievement</h1>
-				<hr className="mb-[5px]"></hr>
-				<ul className="text-[20px]">
-					<li className="flex">
-						<div className="w-[50%]">hyuim</div>
-						<div className="w-[20%]">admin</div>
-						<div className="w-[15%] text-center">23</div>
-						<div className="w-[15%] text-center">40</div>
-					</li>
-					<li className="flex">
-						<div className="w-[50%]">phan</div>
-						<div className="w-[20%]">member</div>
-						<div className="w-[15%] text-center">40</div>
-						<div className="w-[15%] text-center">100</div>
-					</li>
-				</ul>
-			</div>
-			<div className="w-[100%]">
-				<h1 className="text-[25px]">Penalty</h1>
-				<hr className="mb-[5px]"></hr>
-				<ul className="text-[20px]">
-					<li className="flex">
-						<div className="w-[50%]">hyuim</div>
-						<div className="w-[20%]">admin</div>
-						<div className="w-[15%] text-center">3000</div>
-						<div className="w-[15%] text-center">5000</div>
-					</li>
-					<li className="flex">
-						<div className="w-[50%]">soohlee</div>
-						<div className="w-[20%]">manager</div>
-						<div className="w-[15%] text-center">3000</div>
-						<div className="w-[15%] text-center">5000</div>
-					</li>
-					<li className="flex">
-						<div className="w-[50%]">sungmiki</div>
-						<div className="w-[20%]">member</div>
-						<div className="w-[15%] text-center">3000</div>
-						<div className="w-[15%] text-center">5000</div>
-					</li>
-					<li className="flex">
-						<div className="w-[50%]">jonhan</div>
-						<div className="w-[20%]">member</div>
-						<div className="w-[15%] text-center">3000</div>
-						<div className="w-[15%] text-center">5000</div>
-					</li>
-					<li className="flex">
-						<div className="w-[50%]">junssong</div>
-						<div className="w-[20%]">member</div>
-						<div className="w-[15%] text-center">3000</div>
-						<div className="w-[15%] text-center">5000</div>
-					</li>
-				</ul>
-			</div>
-		</div>
+		<AdminBody title={"Dash Board"}>
+			<AdminContentBlock title={"Penalty"} contents={renderPenalties}/>
+		</AdminBody>
 	);
 }
