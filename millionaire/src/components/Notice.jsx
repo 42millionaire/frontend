f041f69aae32ce0d21e8a618c2c6a74e0ecd26fb
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-17 22:03:08 +0900

    refactor: Notice 리펙토링중

--- millionaire/src/components/Notice.jsx
+++ millionaire/src/components/Notice.jsx
@@ -1,36 +1,41 @@
 import { useState } from "react";
+import AdminContentBlock from "./AdminContentBlock";
+import AdminBody from "./AdminBody";
 
 export default function DashBoard() {
 	const [noticeContent, setNoticeContent] = useState("");
 	const handleClick = () => {
 		console.log("HELLO");
 	};
-	return (
-		<div className="w-[55%] mt-[20px]">
-			<h1 className="mb-[10px] text-[40px]">Notice Board</h1>
-			<div className="w-[100%] mb-[50px]">
-				<h1 className="text-[25px]">Notice</h1>
-				<hr className="mb-[5px]" />
-				<p>
-					<textarea
-						className="w-[100%] h-[300px] bg-black"
-						name="notice"
-						placeholder="Notice"
-						value={noticeContent}
-						maxLength={300}
-						onChange={(e) => setNoticeContent(e.target.value)}
+
+	const noticeBox = () =>{
+		return (
+			<>
+			<p>
+				<textarea
+					className="w-[100%] h-[300px] bg-black"
+					name="notice"
+					placeholder="Notice"
+					value={noticeContent}
+					maxLength={300}
+					onChange={(e) => setNoticeContent(e.target.value)}
 					/>
-				</p>
-				<div className="flex justify-end items-center mt-2">
-					<span className="mr-4">{noticeContent.length} / 300</span>
-					<button
-						className="border-[2px] rounded-[10px] px-4 py-1 text-sm"
-						onClick={handleClick}
+			</p>
+			<div className="flex justify-end items-center mt-2">
+				<span className="mr-4">{noticeContent.length} / 300</span>
+				<button
+					className="border-[2px] rounded-[10px] px-4 py-1 text-sm"
+					onClick={handleClick}
 					>
-						Notify
-					</button>
-				</div>
+					Notify
+				</button>
 			</div>
-		</div>
+			</>
+		)
+	}
+	return (
+		<AdminBody title={"Notice Board"}>
+			<AdminContentBlock className={"w-[80%]"} title={"Notice"} contents={noticeBox}/>
+		</AdminBody>
 	);
 }
