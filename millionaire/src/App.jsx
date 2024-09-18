7d6f08af203d8d0680947e4fe41a46e3221ef142
SM Jacob <sm@sms-MacBook-Air.local>
2024-09-18 12:37:39 +0900

    feat: Admin 추가 in App.jsx

--- millionaire/src/App.jsx
+++ millionaire/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./app/Main.jsx";
+import Admin from "./app/Admin.jsx";
 
 function App() {
 	return (
@@ -8,7 +9,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Main />} />
 				{/* <Route path="/login" element={<Login />} /> */}
-				{/* <Route path="/admin" element={<Admin />} /> */}
+				<Route path="/admin" element={<Admin />} />
 			</Routes>
 		</BrowserRouter>
 	);
