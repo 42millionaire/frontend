d10d904e33b58b1edcff212ba0c016d4ae898001
SM Jacob <sm@sms-MacBook-Air.local>
2024-10-22 20:02:25 +0900

    refactor: 그룹이름 실제이름으로 변경

--- millionaire/src/components/Sidebar.jsx
+++ millionaire/src/components/Sidebar.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import PAGE_TITLES from "../constants/PAGE_TITLES.js";
+import BASE_URL from "../constants/URL.js";
 
-const Sidebar = ({ setPage }) => (
+const Sidebar = ({ setPage }) => {
+	const [groupName, setGroupName] = useState("");
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(`${BASE_URL}/group/1`, {
+                    headers: {
+                        "Accept": "application/json",
+                        "ngrok-skip-browser-warning": true,
+                    },
+                    method: "GET",
+                });
+				const res = await response.json();
+				setGroupName(res.groupName);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+	}, []); 
+	return (
 	<div className="w-[23%] mt-[20px] ml-[20px] mr-[10px]">
-		<h1 className="mb-[50px] mt-[20px] text-[30px]">☠️세후1억 해적단☠️</h1>
+		<h1 className="mb-[50px] mt-[20px] text-[30px]">{groupName}</h1>
 		<nav>
 			<ul className="text-[20px]">
 				{Object.entries(PAGE_TITLES).map(([key, title]) => (
@@ -14,6 +37,7 @@ const Sidebar = ({ setPage }) => (
 			</ul>
 		</nav>
 	</div>
-);
+	)
+};
 
 export default Sidebar;
