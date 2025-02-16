9539ce91182795f2b2350f324e55275147c2b420
phan <hanfeelhoo@naver.com>
2025-02-16 18:02:39 +0900

    design: 글자색 변경

--- millionaire/src/app/Home.jsx
+++ millionaire/src/app/Home.jsx
@@ -1,10 +1,10 @@
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-orange-400 text-[50px]">
-			<a href="/main" className="hover:text-black hover:underline">
+			<a href="/main" className="hover:text-orange-200 hover:underline">
 				Main
 			</a>
-			<a href="/admin" className="hover:text-black hover:underline">
+			<a href="/admin" className="hover:text-orange-200 hover:underline">
 				Admin
 			</a>
 		</div>
